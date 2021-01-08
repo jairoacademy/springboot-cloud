e24b42b3f698f2ab8a73ab529997ef7ab00472ff
Jairo soares <jairo.soares@gmail.com>
2021-01-08 15:16:16 -0300

    Add Person Controller

--- src/main/java/academy/jairo/springboot/controller/PersonController.java
+++ src/main/java/academy/jairo/springboot/controller/PersonController.java
@@ -4,7 +4,6 @@ import academy.jairo.springboot.domain.Person;
 import academy.jairo.springboot.util.DateUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
