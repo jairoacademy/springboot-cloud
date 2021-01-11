8ea5ae2b502b70d23daa67f2a12895fdfaabfbc6
Jairo soares <jairo.soares@gmail.com>
2021-01-11 08:48:14 -0300

    Update Person

--- src/main/java/academy/jairo/springboot/domain/Person.java
+++ src/main/java/academy/jairo/springboot/domain/Person.java
@@ -2,6 +2,7 @@ package academy.jairo.springboot.domain;
 
 public class Person {
     private String name;
+    private String email;
 
     public Person(String name) {
         this.name = name;
@@ -13,5 +14,13 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
