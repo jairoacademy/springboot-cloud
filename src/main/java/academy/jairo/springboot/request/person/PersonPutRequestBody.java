0c6d532d8c2986ce3781fd3d53c414106dea07ab
Jairo soares <jairo.soares@gmail.com>
2021-02-02 08:48:26 -0300

    Integration Tests

--- src/main/java/academy/jairo/springboot/request/person/PersonPutRequestBody.java
+++ src/main/java/academy/jairo/springboot/request/person/PersonPutRequestBody.java
@@ -1,10 +1,14 @@
 package academy.jairo.springboot.request.person;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PersonPutRequestBody {
     private Long id;
     private String name;
