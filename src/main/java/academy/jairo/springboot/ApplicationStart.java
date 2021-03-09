e36633344d19e73b4869d9f18a890245c374f983
Jairo Soares <jairo.soares@gmail.com>
2021-03-09 17:15:10 -0300

    feat:redis first commit

--- src/main/java/academy/jairo/springboot/ApplicationStart.java
+++ src/main/java/academy/jairo/springboot/ApplicationStart.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCaching
 public class ApplicationStart {
 
     public static void main(String[] args) {
